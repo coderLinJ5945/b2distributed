a95e1352aa6f1353cf42a78ec63bab5cf0d4884f
linjtest <1293208049@qq.com>
2017-12-20 19:52:29 +0800

    框架配置搭建
    框架配置搭建

---
+++ src/main/java/com/lin/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.lin.dao;
+
+import com.lin.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/lin/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.lin.dao;
+
+import com.lin.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
